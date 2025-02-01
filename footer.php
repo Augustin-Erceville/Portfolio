7e7f789002bf2f74044acccc0c861fd90ed1c3c2
Mitricum <a.erceville2000@gmail.com>
2025-02-01 20:04:55 +0100

    V0.2 • Rédaction du index + Ajout d'une carte d'information.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-4 px-4 border-top fixed-bottom bg-dark" data-bs-theme="dark">
         <div class="col-md-4 d-flex align-items-center">
             <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+                <img src="MitriLogo.png" height="32px">
             </a>
             <span class="mb-3 mb-md-0 text-body-secondary">A.R.C.E &copy; 2025</span>
         </div>
--- header.php
+++ header.php
@@ -5,36 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Portfolio - Augustin d'Erceville</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="styles.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
     </style>
 </head>
-<body style="font-family: Comfortaa">
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+<body style="font-family: Comfortaa;">
+<header class="navbar bg-dark ">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Portfolio</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="./index.php">Acceuil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="presentation.php">Biographie</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown"
-                       aria-expanded="false">BTS SIO</a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="SISR.php">SIO SISR</a></li>
-                        <li><a class="dropdown-item" href="SLAM.php">SIO SLAM</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+        <a class="navbar-brand text-white fs-3 my-2" href="index.php">
+            <img src="MitriLogo.png" alt="Logo" width="32" height="32" class="d-inline-block align-text-top">
+            Mon Portfolio
+        </a>
     </div>
-</nav>
+</header>
