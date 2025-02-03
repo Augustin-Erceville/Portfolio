9a41aabad68f2b872c17f49c43ff7262703b6c90
Mitricum <a.erceville2000@gmail.com>
2025-02-03 08:11:56 +0100

    V0.11 • Répartition des fichiers

---
+++ BTS-SIO.php
@@ -0,0 +1,35 @@
+<p class="fs-5">
+<div class="col card border rounded border-light list-group-item list-group-item text-bg-dark p-3">
+    Le  <abbr title="Brevet de Technicien Supérieur"><button type="button" class="btn btn-outline-secondary btn-sm px-3">BTS</button></abbr> <abbr title="Services Informatiques aux Organisations"><button type="button" class="btn btn-outline-secondary btn-sm px-3">SIO</button></abbr> est une formation en deux ans qui prépare les étudiants aux métiers de l'informatique, notamment dans le développement d'applications et l'administration des systèmes et réseaux.<br>
+    Ce diplôme s'adresse aux passionnés d'informatique souhaitant acquérir des compétences techniques et professionnelles pour intégrer rapidement le marché du travail ou poursuivre leurs études dans le domaine du numérique.
+</div>
+<div class="row my-3">
+    <div class="col m-1 card border rounded border-light list-group-item list-group-item text-bg-dark p-3" style="text-align: justify">
+        <h3 class="text-center"><abbr title="Brevet de Technicien Supérieur"><button type="button" class="btn btn-outline-danger btn-lg px-6">SISR</button></abbr></h3>
+        <hr>
+        <p>L’option <b>S</b>olution d’<b>I</b>nfrastructure, <b>S</b>ystèmes et <b>R</b>éseaux forme des professionnels des réseaux et des équipements informatiques (installation, maintenance, sécurité).</p>
+        Les techniciens supérieurs en informatique, option <b>SISR</b>, peuvent accéder aux métiers de :
+        <li>&nbsp;Administrateur systèmes et réseaux</li>
+        <li>&nbsp;Informaticien support et déploiement</li>
+        <li>&nbsp;Pilote d’exploitation</li>
+        <li>&nbsp;Support systèmes et réseaux</li>
+        <li>&nbsp;Technicien d’infrastructure</li>
+        <li>&nbsp;Technicien de production</li>
+        <li>&nbsp;Technicien micro et réseaux</li>
+    </div>
+    <div class="col m-1 card border rounded border-light list-group-item list-group-item text-bg-dark p-3" style="text-align: justify">
+        <h3 class="text-center"><abbr title="Brevet de Technicien Supérieur"><button type="button" class="btn btn-outline-success btn-lg px-6">SLAM</button></abbr></h3>
+        <hr>
+        <p>L’option <b>S</b>olutions <b>L</b>ogicielles et <b>A</b>pplications <b>M</b>étiers forme des spécialistes des logiciels (rédaction d’un cahier des charges, formulation des besoins et spécifications, développement, intégration au sein de la société).</p>
+        Les techniciens supérieurs en informatique, option <b>SLAM</b>, sont préparés aux métiers de :
+        <li>&nbsp;Développeur d’applications informatiques</li>
+        <li>&nbsp;Développeur informatique</li>
+        <li>&nbsp;Analyste d’applications ou d’études</li>
+        <li>&nbsp;Analyste programmeur</li>
+        <li>&nbsp;Programmeur analyste</li>
+        <li>&nbsp;Programmeur d’applications</li>
+        <li>&nbsp;Responsable des services applicatifs</li>
+        <li>&nbsp;Technicien d’études informatiques</li>
+    </div>
+</div>
+</p>
